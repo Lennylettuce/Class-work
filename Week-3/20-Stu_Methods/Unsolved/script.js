fc70407551a67cb7895ef140b2ab51640ebfa9e4
Aubrey Gunn <agunn369@gmail.com>
2023-02-15 21:23:59 -0600

    edit week 3 day 2

--- Week-3/20-Stu_Methods/Unsolved/script.js
+++ Week-3/20-Stu_Methods/Unsolved/script.js
@@ -5,12 +5,12 @@ var galaxy = [constellations + planets];
 // WRITE YOUR CODE BELOW
 constellations.push("Canis Major");
     console.log(constellations);
-
+//.unshift pulls item front of string
 var planetsSliced = planets.slice(0,5);
     console.log(planetsSliced);
-
+// .pop() removes last from string
 
     console.log(galaxy);
-
+//done right
 var STAR = String(star).toUpperCase();
     console.log(STAR);
