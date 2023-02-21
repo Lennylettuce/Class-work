d260ae9177a1420a2bd0965c584036c810d94064
Aubrey Gunn <agunn369@gmail.com>
2023-02-21 11:46:58 -0600

    added week 4 folder and files, worked on first two ex

---
+++ Week-4/02-Stu_Window-Object/Unsolved/script.js
@@ -0,0 +1,16 @@
+// TODO: What will the following line of code log?
+// console.log(window); will display in the console all the working elements within the browser page
+console.log(window);
+
+// TODO: What will the following line of code log?
+// console.log(window.document); will display/store in the console a copy of the html code which is called a document object model
+console.log(window.document);
+
+// TODO: What will the following line of code log?
+//console.log(document.documentElement); will display in the console the root element of the page - html for html document (had help with this one) 
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log?
+// console.log(document.head); will display the <head> element of the page (help with this too, understood after last example tho lol)
+console.log(document.head);
+
---
+++ Week-4/04-Stu_Traverse-DOM/Unsolved/assets/js/script.js
@@ -0,0 +1,9 @@
+// Access element using id
+var articlesDiv = document.getElementById('articles');
+var headerDiv = document.getElementById('h1');
+//change target to h1, put h1 id in html
+
+// Change style by accessing style object's properties
+articlesDiv.style.fontSize = '50px';
+headerDiv.style.color = 'white';
+ 
