302ee52b7fc1eeb531d0ce7b48e5153cbfdd3d32
Aubrey Gunn <agunn369@gmail.com>
2023-03-09 20:59:27 -0600

    week 6 day 2

--- Week-6/16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
+++ Week-6/16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
@@ -1,9 +1,14 @@
 fetch(
   // Explain each parameter in comments below.
+  //below is the hypertext transfer protocol (secure) which is encrypted (secure)data that is transfered online
+  //below is specific url with specific request parameter: ?per_page put & sign between each parameter
+  //they are like filters for data request responses
+  //sort by create date, direction=desc is list is descending order state=open is show issues with state of open
   'https://api.github.com/repos/nodejs/node/issues?per_page=10&state=open&sort=created&direction=desc'
 )
   .then(function (response) {
     return response.json();
+    //return the response in way javascript can intereperet
   })
   .then(function (data) {
     console.log(data);
--- Week-6/18-Stu__Fetch_Options/Unsolved/assets/js/script.js
+++ Week-6/18-Stu__Fetch_Options/Unsolved/assets/js/script.js
@@ -1,7 +1,21 @@
-fetch('https://api.github.com/repos/nodejs/node/issues?per_page=5')
+fetch('https://api.github.com/repos/nodejs/node/issues?per_page=5', {
+  method: 'GET', //get is default
+  mode: 'cors',
+  cache: 'reload',
+  credentials: 'same-origin',
+}
+)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
   });
+
+  // fetch has documentation too, check that out sometime soon!
+  //authenticate request by passing info in thru an object
+  //send it an auth header in side object
+  //headers:{
+    //authentication: "...."}
+    //inside fetch request, behind url with comma
+  
