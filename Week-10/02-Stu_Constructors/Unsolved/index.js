013a26240410ef4e144174b1e47a93516b35680e
Aubrey Gunn <agunn369@gmail.com>
2023-04-05 19:04:27 -0500

    week 10 added

---
+++ Week-10/02-Stu_Constructors/Unsolved/index.js
@@ -0,0 +1,16 @@
+// TODO: Create a constructor function called 'BlogPost' that takes in 'authorName', 'title', 'text, and 'createdOn'
+    function BlogPost(authorName, title, text, createdOn) {
+        this.authorName = authorName;
+        this.title = title;
+        this.text = text;
+        this.createdOn = createdOn;
+        this.printMetaData = function () {
+            console.log('Created by' + authorName + 'on' + createdOn);
+        };
+    }
+// TODO: Include a method called 'printMetaData()' that prints a message in the console saying 'Created by (authorName) on (createdOn)'
+
+// TODO: Create a new object using the 'BlogPost' constructor
+    const post = new BlogPost('Aub', 'Coding 101', 'here is some text', 'created on 4/5/23');
+// TODO: Call the 'printMetaData()' method on the new object
+    post.printMetaData();
