86c04b24db6eb46710a502b9b2c7ee17d5af3c82
Aubrey Gunn <agunn369@gmail.com>
2023-04-17 22:32:48 -0500

    week 11 day 3

--- Week-11/22-Stu_Modular-Routing/Unsolved/routes/feedback.js
+++ Week-11/22-Stu_Modular-Routing/Unsolved/routes/feedback.js
@@ -2,7 +2,48 @@ const fb = require('express').Router();
 // TODO: Import helper functions and dependencies
 
 // TODO: GET Route for retrieving all the feedback
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+);
+// GET Route for retrieving all the feedback
+app.get('/api/feedback', (req, res) => {
+  console.info(`${req.method} request received for feedback`);
+
+  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)));
+});
+
 
 // TODO: POST Route for submitting feedback
+// POST Route for submitting feedback
+app.post('/api/feedback', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to submit feedback`);
+  
+    // Destructuring assignment for the items in req.body
+    const { email, feedbackType, feedback } = req.body;
+  
+    // If all the required properties are present
+    if (email && feedbackType && feedback) {
+      // Variable for the object we will save
+      const newFeedback = {
+        email,
+        feedbackType,
+        feedback,
+        feedback_id: uuid(),
+      };
+  
+      readAndAppend(newFeedback, './db/feedback.json');
+  
+      const response = {
+        status: 'success',
+        body: newFeedback,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting feedback');
+    }
+  });
 
 module.exports = fb;
--- Week-11/22-Stu_Modular-Routing/Unsolved/routes/index.js
+++ Week-11/22-Stu_Modular-Routing/Unsolved/routes/index.js
@@ -1,9 +1,12 @@
 // TODO: Import express
-
+const express = require('express');
 // TODO: Import modules for tips/feedback
-
+const tipImport = require('./tips');
+const fbRouter = require('./feedback');
 // TODO: Create app vaible to store the value of express()
-
+const api = express();
 // TODO: Use our routes
-
+api.use('/tips', tipImport);
+api.use('./feedback', fbRouter);
 // TODO: Export app
+module.exports = api;
